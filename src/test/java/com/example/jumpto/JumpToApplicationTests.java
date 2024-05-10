0ae39a2a42a5a956dce9776ffcd344c7163a2b30
조용진 <101401582+Brio-yj@users.noreply.github.com>
2024-05-10 22:54:43 +0900

    .

---
+++ src/test/java/com/example/jumpto/JumpToApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jumpto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumpToApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
